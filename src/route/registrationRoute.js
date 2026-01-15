160303422eb2c004ced3e76b191d9501c32bf821
Balint2003 <klubertbalint2003@gmail.com>
2026-01-15 21:12:44 +0100

    HOTFIX: registrationRouter javítása
    A registrationRouterben helytelenül put metódus lett megadva és registrationPUTController post helyett. Ennek a javítása ez a commit.

--- src/route/registrationRoute.js
+++ src/route/registrationRoute.js
@@ -1,11 +1,8 @@
-//Express importálása
 const express = require('express');
 
-//A controller és a Router importálása, inicializálása
 const registrationController = require("../controller/registrationController");
 const registrationRouter = express.Router();
 
-//Route-ok létrehozása
-registrationRouter.put("/registration", registrationController.registrationPUTController);
+registrationRouter.post("/registration", registrationController.registrationPOSTController);
 
 module.exports = registrationRouter;
